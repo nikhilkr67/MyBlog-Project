5692d33de39f1fba9f49a38c9a772b507de8721f
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-22 00:01:15 +0530

    Add files via upload

--- service/PostService.java
+++ service/PostService.java
@@ -1,1 +1,19 @@
+package com.myblog.service;
 
+import com.myblog.payload.PostDto;
+import com.myblog.payload.PostResponse;
+
+import java.util.List;
+
+public interface PostService {
+
+    PostDto createPost(PostDto postDto);
+
+
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
+
+    PostDto getPostById(long id);
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
